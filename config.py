28a61b3caef1d78edbc40282c3023bc2e4b2ac2f
George Daniel Arteni <arteni.george.daniel@gmail.com>
2022-07-16 18:42:38 +0300

    changed the default db type for lucky-SIX- to v2

--- config.py
+++ config.py
@@ -84,7 +84,7 @@ assets = {'XSE': {'db_filepath': '{userdir}\\.seno2\\mainnet\\db\\blockchain_v2_
          'SIT': {'db_filepath': '{userdir}\\.sit\\mainnet\\db\\blockchain_v1_mainnet.sqlite'.format(userdir=path.expanduser("~")),
                  'denominator': 1000000000000,
                  'friendly_name': 'silicoin'},
-         'SIX': {'db_filepath': '{userdir}\\.lucky\\mainnet\\db\\blockchain_v1_mainnet.sqlite'.format(userdir=path.expanduser("~")),
+         'SIX': {'db_filepath': '{userdir}\\.lucky\\mainnet\\db\\blockchain_v2_mainnet.sqlite'.format(userdir=path.expanduser("~")),
                  'denominator': 1000000000000,
                  'friendly_name': 'lucky'},
          'SOCK': {'db_filepath': '{userdir}\\.socks\\mainnet\\db\\blockchain_v1_mainnet.sqlite'.format(userdir=path.expanduser("~")),
