65d4c20e13f9ce87d9971fb4bfb0b7a62e10ec25
Arteni George Daniel <arteni.george.daniel@gmail.com>
2022-09-24 13:52:15 +0300

    added support for moon (MOC)

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 from os import path
 
-assets = {'HCX': {'db_filepath': '{userdir}\\.chinilla\\vanillanet\\db\\blockchain_v2_vanillanet.sqlite'.format(userdir=path.expanduser("~")),
+assets = {'MOC': {'db_filepath': '{userdir}\\.moon\\mainnet\\db\\blockchain_v2_mainnet.sqlite'.format(userdir=path.expanduser("~")),
+                 'denominator': 1000000000000,
+                 'friendly_name': 'moon'},
+          'HCX': {'db_filepath': '{userdir}\\.chinilla\\vanillanet\\db\\blockchain_v2_vanillanet.sqlite'.format(userdir=path.expanduser("~")),
                  'denominator': 1000000000000,
                  'friendly_name': 'chinilla'},
           'XSE': {'db_filepath': '{userdir}\\.seno2\\mainnet\\db\\blockchain_v2_mainnet.sqlite'.format(userdir=path.expanduser("~")),
